19008f418b861ee98a2848048c8c0002e4fdac69
gabscar <srgabsc@gmail.com>
2021-11-11 09:27:24 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,13 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
-  handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+  handle (httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return badRequest(new MissingParamError('name'))
+    }
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
