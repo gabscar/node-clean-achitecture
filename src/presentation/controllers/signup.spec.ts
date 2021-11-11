1798b8219b8d8936d01ab4680ff349c646318d59
gabscar <srgabsc@gmail.com>
2021-11-11 11:00:23 -0300

    refactor: move all imports errors to a single archive

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,5 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
 import { ServerError } from '../errors/server-error'
 
