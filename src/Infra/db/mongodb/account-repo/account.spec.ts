8dfb1047d714208be188f603801d58ef90fd7b93
gabscar <srgabsc@gmail.com>
2021-11-12 12:38:06 -0300

    test: adding tests MongoAccountRepository

---
+++ src/Infra/db/mongodb/account-repo/account.spec.ts
@@ -0,0 +1,24 @@
+import { MongoHelper } from '../helpers/mongo-helpers'
+import { AccountMongoRepository } from './account'
+describe('Account MongoDB repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.close()
+  })
+  test('should retorn account on sucess', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password'
+    })
+    expect(account).toBeTruthy()
+    const { name, email, password, id } = account
+    expect(id).toBeTruthy()
+    expect(name).toBe('name')
+    expect(email).toBe('email@mail.com')
+    expect(password).toBe('password')
+  })
+})
