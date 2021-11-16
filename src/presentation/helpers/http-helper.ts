589e3e334dbfdf3eeefe712c9c8b4d17d753ee88
gabscar <srgabsc@gmail.com>
2021-11-16 14:50:30 -0300

    feat: add authentication and unauthorized error

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,11 +1,14 @@
 import { HttpResponse } from '../protocols/http'
-import { ServerError } from '../errors'
+import { ServerError, UnautorizedError } from '../errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
-
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnautorizedError()
+})
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
