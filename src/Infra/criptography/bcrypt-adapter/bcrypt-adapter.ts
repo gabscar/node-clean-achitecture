feb788fe5c4b47af9e3346f5c1b8f1981ece06ee
gabscar <srgabsc@gmail.com>
2021-11-17 17:52:15 -0300

    feat: add JwtAdapter Methods

--- src/Infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/Infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
-import { Hasher } from '../../data/protocols/criptography/hasher'
+import { Hasher } from '../../../data/protocols/criptography/hasher'
 import bcrypt from 'bcrypt'
-import { HashCompare } from '../../data/protocols/criptography/hash-comparer'
+import { HashCompare } from '../../../data/protocols/criptography/hash-comparer'
 export class BcryptAdapter implements Hasher, HashCompare {
   private readonly saltBcryptParam: number
 
