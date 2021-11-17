3d4f24e85c5a1d1ca4d18793f038a838d48666f0
gabscar <srgabsc@gmail.com>
2021-11-17 19:21:16 -0300

    test: account implementation LoadAccountByEmailRepository and UpdateAccessTokenRepository

--- src/Infra/db/mongodb/account-repo/account.spec.ts
+++ src/Infra/db/mongodb/account-repo/account.spec.ts
@@ -1,5 +1,9 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helpers'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
+
 describe('Account MongoDB repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -8,7 +12,7 @@ describe('Account MongoDB repository', () => {
     await MongoHelper.close()
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
   test('should retorn account on add sucess', async () => {
@@ -24,5 +28,39 @@ describe('Account MongoDB repository', () => {
     expect(name).toBe('name')
     expect(email).toBe('email@mail.com')
     expect(password).toBe('password')
+  })
+  test('should retorn account on loadByEmail sucess', async () => {
+    const sut = new AccountMongoRepository()
+    await accountCollection.insertOne({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password'
+    })
+    const account = await sut.loadByEmail('email@mail.com')
+    expect(account).toBeTruthy()
+    const { name, email, password, id } = account
+    expect(id).toBeTruthy()
+    expect(name).toBe('name')
+    expect(email).toBe('email@mail.com')
+    expect(password).toBe('password')
+  })
+  test('should retorn null if loadByEmail fail', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.loadByEmail('email@mail.com')
+    expect(account).toBeFalsy()
+  })
+  test('Should update the account accessToken on updateAccesToken succces', async () => {
+    const sut = new AccountMongoRepository()
+    const response = await accountCollection.insertOne({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password'
+    })
+    const fakeAccount = response.ops[0]
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
   })
 })
