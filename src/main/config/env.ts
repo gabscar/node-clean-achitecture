e13b418ba40146dd8aeda13743c3664af18b5e03
gabscar <srgabsc@gmail.com>
2021-11-19 12:41:43 -0300

    refactor: change link to mongo database

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://gabscar:GtpWsZYURlST03Pv@cluster0.qi6v5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tjk310=2G'
 }
